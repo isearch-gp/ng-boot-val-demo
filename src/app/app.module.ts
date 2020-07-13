d699f1a6ec7c9f2d5055eea095707a9860045f97
mkobar <mike@exlorev.com>
2020-07-13 14:04:11 -0400

    working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
+import { BasicExampleComponent } from './components/basic-example/basic-example.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgBootstrapFormValidationModule.forRoot(),
+    NgBootstrapFormValidationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/basic-example/basic-example.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-basic-example',
+  templateUrl: './basic-example.component.html',
+  styleUrls: ['./basic-example.component.css']
+})
+export class BasicExampleComponent implements OnInit {
+
+  formGroup: FormGroup;
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      Email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      Password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
+  }
+
+  onReset() {
+    this.formGroup.reset();
+  }
+
+}
